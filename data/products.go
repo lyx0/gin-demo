87cbfe5c24df8d61d2f419f7d031a7dba15d2502
lyx0 <stefan@parfuss.me>
2021-07-23 00:54:10 +0200

    idk

--- data/products.go
+++ data/products.go
@@ -1,8 +1,6 @@
 package data
 
 import (
-	"encoding/json"
-	"io"
 	"time"
 )
 
@@ -11,17 +9,15 @@ type Product struct {
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float32 `json:"price"`
-	SKU         string  `json:"-"`
+	SKU         string  `json:"sku"`
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeletedOn   string  `json:"-"`
 }
-
 type Products []*Product
 
-func (p *Products) ToJSON(w io.Writer) error {
-	e := json.NewEncoder(w)
-	return e.Encode(p)
+func GetProducts() Products {
+	return productList
 }
 
 var productList = []*Product{
@@ -44,3 +40,8 @@ var productList = []*Product{
 		UpdatedOn:   time.Now().UTC().String(),
 	},
 }
+
+// func (p *Products) ToJSON(w io.Writer) error {
+// 	e := json.NewEncoder(w)
+// 	return e.Encode(p)
+// }
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lylx0/gin-demo/data"
+)
 
 func main() {
 	r := gin.Default()
@@ -9,5 +12,7 @@ func main() {
 			"message": "pong",
 		})
 	})
+	getProd = data.GetProducts()
+	r.GET("/products", getProd)
 	r.Run()
 }
