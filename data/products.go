a5baaee0503287a59491477241733fff5c2cd28c
lyx0 <stefan@parfuss.me>
2021-07-25 19:09:03 +0200

    add products handler

---
+++ data/products.go
@@ -0,0 +1,46 @@
+package data
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Product defines the structure for an API product
+type product struct {
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+
+
+func GetProducts(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, productList)
+}
+
+var productList = []product{
+	{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc123",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
--- main.go
+++ main.go
@@ -1,32 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"net/http"
-	"time"
-
 	"github.com/gin-gonic/gin"
+	"github.com/lylx0/gin-demo/data"
 )
 
-func formatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d%02d/%02d", year, month, day)
-}
-
 func main() {
-	router := gin.Default()
-	router.Delims("{[{", "}]}")
-	router.SetFuncMap(template.FuncMap{
-		"formatAsDate": formatAsDate,
-	})
-	router.LoadHTMLFiles("./testdata/raw.tmpl")
-
-	router.GET("/raw", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "raw.tmpl", map[string]interface{}{
-			"now": time.Date(2017, 07, 01, 0, 0, 0, 0, time.UTC),
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
 		})
 	})
-
-	router.Run(":8080")
+	r.GET("/products", data.GetProducts)
+	r.Run()
 }
